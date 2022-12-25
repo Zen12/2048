7df819c3cb87e92e8207ca3746d3fb65008213df
Zen12 <andrei1292@gmail.com>
2022-12-25 16:45:05 +0200

    Sprite animation

--- Assets/_Project/Scripts/View/SpriteView.cs
+++ Assets/_Project/Scripts/View/SpriteView.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using MonoDI.Scripts.Core;
 using UnityEngine;
 
@@ -8,17 +10,49 @@ namespace _Project.Scripts.View
         [Get] private SpriteRenderer _sprite;
         [Get] private Transform _tr;
 
+        private Vector3 _targetPos;
+        private Coroutine _lastRoutine;
+        private System.Action _onFinishMove;
+
         public Vector2Int GridPos
         {
             get;
             private set;
         }
 
+        private float _speed = 12f;
 
-        public void UpdatePosition(Vector3 pos, Vector2Int gridPos)
+        public void UpdateOnFinish(System.Action onFinish)
         {
-            _tr.position = pos;
+            _onFinishMove = onFinish;
+        }
+
+
+        public void Animate(Vector3 pos, Vector2Int gridPos, AnimationType animationType,
+            System.Action onFinish = null)
+        {
+            gameObject.SetActive(true);
+            _onFinishMove = onFinish;
             GridPos = gridPos;
+            if (_lastRoutine != null)
+                StopCoroutine(_lastRoutine);
+
+            switch (animationType)
+            {
+                case AnimationType.Move:
+                    _targetPos = pos;
+                    _lastRoutine = StartCoroutine(MoveToPosRoutine());
+                    break;
+                case AnimationType.Create:
+                    _targetPos = pos;
+                    _lastRoutine = StartCoroutine(CreateRoutine());
+                    break;
+                case AnimationType.Disappear:
+                    _lastRoutine = StartCoroutine(DisappearRoutine());
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(animationType), animationType, null);
+            }
         }
         public void UpdateSprite(Sprite sprite)
         {
@@ -27,12 +61,109 @@ namespace _Project.Scripts.View
         
         public void OnEnterPool()
         {
-            gameObject.SetActive(false);
         }
 
         public void OnExitPool()
         {
-            gameObject.SetActive(true);
+        }
+        
+        #region Animation
+
+        private IEnumerator DisappearRoutine()
+        {
+            var delta = Time.deltaTime * _speed;
+            var dir = _targetPos - _tr.position;
+
+            while(dir.magnitude > delta + float.Epsilon)
+            {
+                _tr.position += dir.normalized * delta;
+                yield return null;
+                delta = Time.deltaTime * _speed;
+                dir = _targetPos - _tr.position;
+            }
+            
+            _tr.position = _targetPos;
+            
+            var currentLerp = 0f;
+
+            //grow
+            while (currentLerp < 1f)
+            {
+                currentLerp += Time.deltaTime * 4f;
+                _tr.localScale = Vector3.Lerp(Vector3.zero, Vector3.zero, currentLerp);
+                yield return null;
+            }
+            
+            _lastRoutine = null;
+            _onFinishMove?.Invoke();
+            gameObject.SetActive(false);
+        }
+
+        private IEnumerator MoveToPosRoutine()
+        {
+            yield return null;
+            var delta = Time.deltaTime * _speed;
+            var dir = _targetPos - _tr.position;
+
+            while(dir.magnitude > delta + float.Epsilon)
+            {
+                _tr.position += dir.normalized * delta;
+                yield return null;
+                delta = Time.deltaTime * _speed;
+                dir = _targetPos - _tr.position;
+            }
+
+            _tr.position = _targetPos;
+            _lastRoutine = null;
+            _onFinishMove?.Invoke();
+        }
+        
+        private IEnumerator CreateRoutine()
+        {
+            _tr.position = _targetPos;
+            _tr.localScale = Vector3.zero;
+
+            var currentLerp = 0f;
+
+            var big = Vector3.one * 1.1f;
+            var small = Vector3.one * 0.9f;
+            var speed = 4f;
+
+            //grow
+            while (currentLerp < 1f)
+            {
+                currentLerp += Time.deltaTime * speed * 4f;
+                _tr.localScale = Vector3.Lerp(Vector3.zero, big, currentLerp);
+                yield return null;
+            }
+
+            currentLerp = 0f;
+            //small 
+            while (currentLerp < 1f)
+            {
+                currentLerp += Time.deltaTime * speed;
+                _tr.localScale = Vector3.Lerp(big, small, currentLerp);
+                yield return null;
+            }
+
+            currentLerp = 0f;
+
+            //normal 
+            while (currentLerp < 1f)
+            {
+                currentLerp += Time.deltaTime * speed;
+                _tr.localScale = Vector3.Lerp(small, Vector3.one, currentLerp);
+                yield return null;
+            }
+            
+            _onFinishMove?.Invoke();
+        }
+        
+        #endregion
+        
+        public enum AnimationType
+        {
+            Move, Create, Disappear
         }
     }
 }
